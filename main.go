a795585e73e5e8617d3fca704d1f308fa0ceee89
Prasanta Singh <singh.prasanta@gmail.com>
2019-06-24 01:47:20 +0530

    custome greeting message passed using -m "msg"

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -11,14 +12,26 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!\n")
 }
 
+type MsgHandler struct {
+	Msg string
+}
+
+func (mh *MsgHandler) greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s", mh.Msg)
+}
+
 func main() {
 	port := flag.Int("p", 80, "Port")
+	msg := flag.String("m", "Message", "-m \"Welcome!\"")
 	flag.Parse()
+
+	mh := &MsgHandler{Msg: *msg}
 
 	log.Println(fmt.Sprintf("Listening on port %v...", *port))
 
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":80", nil)
+	http.HandleFunc("/greet", mh.greet)
 	//http.ListenAndServe(":80", nil)
 	log.Fatal(http.ListenAndServe(
 		fmt.Sprintf(":%v", *port), nil))
